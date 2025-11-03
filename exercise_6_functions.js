b93b37c6f0470f539044d400c5403a1c1d9275af
raihanaaaxoxo-oss <raihanaaaxoxo@gmail.com>
2025-11-03 11:13:49 +0200

    practicing functions that take parameters, return values, and perform specific tasks.

---
+++ exercise_6_functions.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    return `hello, ${name}!`;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+const greeting = greet("raihana");
+const sumResult = add(6, 4);
+
+console.log(greeting);
+console.log("the sum of 6 and 4 is " + sumResult);
